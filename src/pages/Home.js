05f879807b6c31a520dd5b72421a9a88d1057269
puakehaulani <puakehaulani@gmail.com>
2021-08-02 13:43:37 -0500

    content update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+
 
 
 export default function Home() {
     return (
-        <div className="row d-flex justify-content-center entrance">
-            <Link
-                to="/portfolio-react/about"
-                className="text-center"
-            >
-                aloha e honua
-                <br />
-                hello world
-            </Link>
+        <div className="row d-flex entrance">
+            Hey, I'm Michael. <br />
+            I like dogs.
         </div>
     )
 }
