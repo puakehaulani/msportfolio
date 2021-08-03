159037f45763781aa0789ca509627f697f43ca67
puakehaulani <puakehaulani@gmail.com>
2021-08-02 19:35:54 -0500

    added attribute

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Container(props) {
-    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
+    return (
+        <> <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />
+            <span className="attribute">created with &lt;3 by lexijack</span></>
+        //https://github.com/puakehaulani
+    )
 }
 
 export default Container;
