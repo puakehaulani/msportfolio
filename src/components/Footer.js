a40917d1fef5523b9e8188f7e38586c2429ab19a
puakehaulani <puakehaulani@gmail.com>
2021-08-02 19:36:13 -0500

    added link to admin sign in place holder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
     return (
-        <footer id="contact" className="footer mt-auto py-3 text-center">
+        <footer id="contact" className="footer mt-auto pb-2 ">
+            <div className="text-center">
 
-            <a className="px-4" href="https://github.com/michaelscales88" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithubAlt} size="2x" /></a>
-            <a className="px-4" href="https://www.linkedin.com/in/michael-scales/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="2x" /></a>
-            <a className="px-4" href="https://www.instagram.com/ladyaliceandmike/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
-
+                <a className="px-4" href="https://github.com/michaelscales88" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithubAlt} size="2x" /></a>
+                <a className="px-4" href="https://www.linkedin.com/in/michael-scales/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="2x" /></a>
+                <a className="px-4" href="https://www.instagram.com/ladyaliceandmike/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+                <a className="px-4" href="https://www.google.com" target="_self" rel="noreferrer"><FontAwesomeIcon icon={faUserAstronaut} size="2x" /></a>
+                {/* above is the link to the admin login */}
+            </div>
         </footer>
     );
 }
