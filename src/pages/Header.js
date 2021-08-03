099e78349d612c6ed558c255f3eddeeb081d1fe6
puakehaulani <puakehaulani@gmail.com>
2021-08-02 19:36:27 -0500

    neon text styling

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <div className="row d-flex entrance">
+        <div className="row d-flex entrance neonText">
             Hey, I'm Michael. <br />
             I like dogs.
         </div>
