2a1cb2ed7833c5f4e15e8ff1240f4e7373148bcb
puakehaulani <puakehaulani@gmail.com>
2021-08-04 15:17:33 -0500

    added padding to extend parallax showing

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,13 +4,12 @@ import Container from '../components/Container';
 export default function Header() {
     return (
         <Container>
-            <div className="d-flex justify-content-around">
+            <div id="header" className="d-flex justify-content-around">
                 <div className="entrance neonText">
                     Hey, I'm Michael.<br />
                     I like dogs.
                 </div>
                 <img src="./images/self.png" alt="self portrait" height="300rem" width="300rem" />
-
             </div>
 
         </Container>
