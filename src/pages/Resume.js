80ed6f08e01fd3c6e9bafc7ad56e4034f6688a89
puakehaulani <puakehaulani@gmail.com>
2021-08-04 10:45:52 -0500

    positioning on button and header

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function Resume() {
     return (
-        <div id="resume" className="row d-flex mt-5">
-            <div className="d-flex justify-content-end text-nowrap"><h3>Resume</h3></div>
-
-            <div className="jumbotron col-md-10 bg-dark adjustRight">
-                <div className="d-flex justify-content-between flex-wrap">
-                    <img src="./images/resumeimg.png" alt="resume" />
-                    <a className="btn btn-danger align-self-baseline" role="button" href="./resume.pdf"
+        <div id="resume" className="col d-flex mt-5">
+            <div className="jumbotron col-md-10 bg-dark adjustRight resume">
+                <div className="d-flex justify-content-end text-nowrap rightHeader"><h1>Resume</h1></div>
+                <div className="d-flex flex-wrap resumeImg">
+                    <img src="./images/resumeimg.png" alt="resume" className="mx-auto py-auto" />
+                    <a className="btn btn-danger align-self-start mt-5 mr-5 resumeButton" role="button" href="./resume.pdf"
                         download="resume.pdf">
                         Download
                     </a>
