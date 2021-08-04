298870f7422a7b237e9fd32cc4f1421d6474d0a3
puakehaulani <puakehaulani@gmail.com>
2021-08-04 15:11:01 -0500

    added parallax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Parallax } from 'react-parallax';
+
 import Container from "./components/Container";
 import NavTabs from "./components/NavTabs";
 import Header from "./pages/Header";
@@ -7,21 +9,40 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
-
+import Background from "./assets/images/bkg.jpg";
+import Sample from "./assets/images/sample.jpg";
 
 function App() {
   return (
     <Router>
       <>
-        <Container>
-          <NavTabs />
-          {/* <Route path="/" component={Header} /> */}
+
+        <NavTabs />
+        {/* <Route path="/" component={Header} /> */}
+        <Parallax
+          bgImage={Background}
+          strength={500}
+        >
           <Header />
+        </Parallax>
+        {/* <Container> */}
+        <Parallax
+          bgImage={Sample}
+          strength={500}>
           <About />
+        </Parallax>
+        <Parallax
+          bgImage={Sample}
+          strength={500}>
           <Projects />
+        </Parallax>
+        <Parallax
+          bgImage={Sample}
+          strength={500}>
           <Resume />
-          <Footer />
-        </Container>
+        </Parallax>
+        <Footer />
+        {/* </Container> */}
       </>
     </Router>
   );
