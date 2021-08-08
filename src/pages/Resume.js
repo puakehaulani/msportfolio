6c005604ba7565cef846c46391b6ac3ca44d6bed
puakehaulani <puakehaulani@gmail.com>
2021-08-08 16:26:52 -0400

    added resume to download and moved button to top

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,15 +5,17 @@ export default function Resume() {
     return (
         <Container>
             <div id="resume" className="col d-flex mt-5">
+
                 <div className="jumbotron col-md-10 bg-dark adjustRight resume">
-                    <div className="d-flex justify-content-end text-nowrap rightHeader"><h1>Resume</h1></div>
-                    <div className="d-flex flex-wrap resumeImg">
-                        <img src="./images/resumeimg.png" alt="resume" className="mx-auto py-auto" />
-                    </div>
+                    <div className="d-flex justify-content-end neonText rightHeader"><h1>Resume</h1></div>
                     <a className="btn btn-danger align-self-start mt-5 mr-5 resumeButton" role="button" href="./resume.pdf"
                         download="resume.pdf">
                         Download
                     </a>
+                    <div className="d-flex flex-wrap resumeImg">
+                        <img src="./images/resumeimg.png" alt="resume" className="mx-auto py-auto" />
+                    </div>
+
                 </div>
             </div >
         </Container>
