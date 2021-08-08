ab781249848332f80bef6d1ba9da5be89492f5a3
puakehaulani <puakehaulani@gmail.com>
2021-08-08 16:30:57 -0400

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Parallax } from 'react-parallax';
 
-// import Container from "./components/Container";
 import NavTabs from "./components/NavTabs";
 import Header from "./pages/Header";
 import About from "./pages/About";
@@ -10,9 +9,6 @@ import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
 import Background from "./assets/images/bkg.jpg";
-import Sample from "./assets/images/sample.jpg";
-import Parallax1 from "./assets/images/parallax1.jpg";
-import Parallax2 from "./assets/images/parallax2.jpg";
 import Parallax3 from "./assets/images/parallax3.jpg";
 
 function App() {
