f00ca891d7f0ce88011f98634e0eb3313593a6b2
puakehaulani <puakehaulani@gmail.com>
2021-08-17 13:07:40 -0700

    formatting

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,10 @@ import Container from "react-bootstrap/Container";
 function About() {
     return (
         <Container fluid="true" id="about" className="jumbotron bg-dark adjustRight mt-5">
-            <h1 className="d-flex justify-content-end rightHeader neonText">About me</h1>
+            <h1 className="d-flex justify-content-end rightHeader neonText">
+                About me
+            </h1>
+
             <div className="sectionContent">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
