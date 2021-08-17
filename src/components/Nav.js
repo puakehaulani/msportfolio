d2af9dd6116b23d0086bbbc758a81b6512a9d02b
puakehaulani <puakehaulani@gmail.com>
2021-08-17 10:30:25 -0700

    removed unused components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,27 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
     return (
         <Navbar bg="transparent" expand="lg" fixed="top">
-            <Container>
+            <ul className="flex-column">
                 <Navbar.Brand href="#">
                     <img src="../images/logo.png" width="120" height="80" alt="michael scales logo" />
                 </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
+
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#resume">Resume</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
+
                 </Navbar.Collapse>
-            </Container>
+            </ul>
         </Navbar>
     )
 }
