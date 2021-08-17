7f86fcbfe0e47f5639efeaba1bdb94758a9c5f8e
puakehaulani <puakehaulani@gmail.com>
2021-08-17 13:08:03 -0700

    refactored with bootstrap pkg, removed redundancy

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Container from "../components/Container";
+import Container from "react-bootstrap/Container";
 import ProjectCard from '../components/ProjectCard';
 
 function Projects() {
-
     return (
-        <Container>
-            <div id="projects" className="row mt-5">
-                <div className="jumbotron col-md-10 bg-dark adjustLeft">
-                    <div className="d-flex justify-content-start neonText leftHeader"><h1>Projects</h1></div>
-                    <Container className="sectionContent">
-                        <ProjectCard
-                        />
-                    </Container>
-                </div>
+        <Container fluid="true" id="projects" className="jumbotron bg-dark adjustLeft mt-5">
+            <h1 className="d-flex justify-content-start neonText leftHeader">
+                Projects
+            </h1>
+
+            <div className="sectionContent">
+                <ProjectCard />
             </div>
         </Container>
     )
