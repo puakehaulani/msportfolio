3111d7a96fd80dac831995f68680e1f66cfd5701
puakehaulani <puakehaulani@gmail.com>
2021-08-17 10:37:03 -0700

    added li to make spacing match when collapsed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,21 +6,25 @@ function Navigation() {
     return (
         <Navbar bg="transparent" expand="lg" fixed="top">
             <ul className="flex-column">
-                <Navbar.Brand href="#">
-                    <img src="../images/logo.png" width="120" height="80" alt="michael scales logo" />
-                </Navbar.Brand>
+                <li>
+                    <Navbar.Brand href="#">
+                        <img src="../images/logo.png" width="120" height="80" alt="michael scales logo" />
+                    </Navbar.Brand>
+                </li>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <li>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
 
-                    <Nav className="me-auto">
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#resume">Resume</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#projects">Projects</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#resume">Resume</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
 
-                </Navbar.Collapse>
+                    </Navbar.Collapse>
+                </li>
             </ul>
         </Navbar>
     )
