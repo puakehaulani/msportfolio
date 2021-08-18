2b578d522f5f239beaeba2eca9af937ead84362d
puakehaulani <puakehaulani@gmail.com>
2021-08-18 16:08:18 -0700

    updated spacing with pkg syntax

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 
 export default function Resume() {
     return (
         <Container fluid="true" id="resume" className="jumbotron bg-dark adjustRight mt-5">
-            <h1 className="d-flex justify-content-end neonText rightHeader">
-                Resume
-            </h1>
 
-            <Button variant="danger" className="align-self-start mt-5 mr-5 resumeButton" role="button" href="./resume.pdf"
-                download="resume.pdf">
-                Download
-            </Button>
+            <h1 className="d-flex justify-content-end neonText rightHeader">Resume</h1>
 
-            <div className="d-flex flex-wrap resumeImg">
-                <img src="./images/resumeimg.png" alt="resume" className="mx-auto py-auto" />
+            <div className="sectionContent">
+                <Button className="btn btn-danger align-self-start mt-5 mr-5 resumeButton" role="button" href="./resume.pdf"
+                    download="resume.pdf">
+                    Download
+                </Button>
+
+                <Image src="./images/resumeimg.png" alt="resume" className="resumeImg" />
             </div>
-
         </Container >
     )
 }
