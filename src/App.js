9516409edc534e3bd196019ab78e5bfc01214674
puakehaulani <puakehaulani@gmail.com>
2021-08-20 16:40:07 -0700

    moved files to match structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import { Parallax } from 'react-parallax';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import { Element } from 'react-scroll';
 
-
-import Navigation from "./components/Nav";
-import Header from "./pages/Header";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Resume from "./pages/Resume";
-import Footer from "./components/Footer";
-import Background from "./assets/images/bkg.jpg";
-import Parallax3 from "./assets/images/parallax3.jpg";
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
-      <Navigation />
       <Container fluid="true">
-        <Parallax
-          bgImage={Background}
-          strength={550}>
-          <Element name="header" className="Element"><Header /></Element>
-        </Parallax>
-
-        <Parallax
-          bgImage={Parallax3}
-          strength={550}>
-          <Element name="about" className="Element"><About /></Element>
-          <Element name="projects" className="Element"><Projects /></Element>
-          <Element name="resume" className="Element"><Resume /></Element>
-        </Parallax>
-        <Element name="contact"><Footer /></Element>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
       </Container>
       <span className="attribute">created with &lt;3 by lexijack</span>
     </Router >
---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Parallax } from 'react-parallax';
+import { Element } from 'react-scroll';
+
+import Navigation from "../components/Nav";
+import Header from "../components/Header";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Resume from "../components/Resume";
+import Footer from "../components/Footer";
+import Background from "../assets/images/bkg.jpg";
+import Parallax3 from "../assets/images/parallax3.jpg";
+
+const Home = () => {
+    return (
+        <>
+            <Navigation />
+            <Parallax
+                bgImage={Background}
+                strength={550}>
+                <Element name="header" className="Element"><Header /></Element>
+            </Parallax>
+
+            <Parallax
+                bgImage={Parallax3}
+                strength={550}>
+                <Element name="about" className="Element"><About /></Element>
+                <Element name="projects" className="Element"><Projects /></Element>
+                <Element name="resume" className="Element"><Resume /></Element>
+            </Parallax>
+            <Element name="contact"><Footer /></Element>
+        </>
+    )
+}
+
+export default Home
