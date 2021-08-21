8681e0dc0efa93d15b0540f206687724998f248f
puakehaulani <puakehaulani@gmail.com>
2021-08-21 13:19:19 -0700

    replaced fontawesome package with single icon pkg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import { FaGithubAlt, FaLinkedinIn, FaInstagram, FaUserAstronaut } from 'react-icons/fa'
 
 function Footer() {
     return (
         <footer id="contact" className="footer mt-auto pb-2 ">
             <div className="text-center">
 
-                <a className="px-4" href="https://github.com/michaelscales88" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithubAlt} size="2x" /></a>
-                <a className="px-4" href="https://www.linkedin.com/in/michael-scales/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} size="2x" /></a>
-                <a className="px-4" href="https://www.instagram.com/ladyaliceandmike/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
-                <a className="px-4" href="http://localhost:3000/login" target="_self" rel="noreferrer"><FontAwesomeIcon icon={faUserAstronaut} size="2x" /></a>
+                <a className="px-4" href="https://github.com/michaelscales88" target="_blank" rel="noreferrer"><FaGithubAlt size="2em" /></a>
+                <a className="px-4" href="https://www.linkedin.com/in/michael-scales/" target="_blank" rel="noreferrer"><FaLinkedinIn size="2em" /></a>
+                <a className="px-4" href="https://www.instagram.com/ladyaliceandmike/" target="_blank" rel="noreferrer"><FaInstagram size="2em" /></a>
+                <a className="px-4" href="http://localhost:3000/login" target="_self" rel="noreferrer"><FaUserAstronaut size="2em" /></a>
                 {/* above is the link to the admin login */}
             </div>
         </footer>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { useGoogleLogin } from 'react-google-login';
+import { FcGoogle } from 'react-icons/fc';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
-const Login = () => {
+// refresh token
+// import { refreshTokenSetup } from '../utils/refreshToken';
+
+const clientId =
+    '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com';
+
+function Login() {
+    const onSuccess = (res) => {
+        console.log('Login Success: currentUser:', res.profileObj);
+        alert(
+            `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+        );
+        // refreshTokenSetup(res);
+    };
+
+    const onFailure = (res) => {
+        console.log('Login failed: res:', res);
+        alert(
+            `Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz`
+        );
+    };
+
+    const { signIn } = useGoogleLogin({
+        onSuccess,
+        onFailure,
+        clientId,
+        isSignedIn: true,
+        accessType: 'offline',
+        // responseType: 'code',
+        // prompt: 'consent',
+    });
+
     return (
-        <h1>Login</h1>
-    )
+        <Container><Button onClick={signIn} variant="light" size="lg">
+            <span className="buttonText"><FcGoogle /> Sign in with Google</span>
+        </Button>
+        </Container>
+    );
 }
 
-export default Login
+export default Login;
