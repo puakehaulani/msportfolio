8e1d14d5553a2518564d6507ecbc8e9887decb3b
puakehaulani <puakehaulani@gmail.com>
2021-08-21 13:19:42 -0700

    included routes to pgs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Logout from './pages/Logout';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Container fluid="true">
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/dashboard" component={Logout} />
       </Container>
       <span className="attribute">created with &lt;3 by lexijack</span>
     </Router >
