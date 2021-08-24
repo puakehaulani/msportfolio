d967e2881b3f8bf9131ebd9c2433ad383f6254ea
puakehaulani <puakehaulani@gmail.com>
2021-08-24 10:53:02 -0700

    renamed db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,14 +16,14 @@ if (process.env.NODE_ENV === "production") {
 // app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/msportfolio", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
 
-}).then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
+// }).then(() => console.log("Database Connected Successfully"))
+//     .catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function () {
