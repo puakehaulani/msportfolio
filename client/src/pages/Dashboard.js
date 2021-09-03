928761999b94cf8c5ab5a5011bac24ea7efe69f6
puakehaulani <puakehaulani@gmail.com>
2021-09-03 10:53:50 -0700

    file upload ui

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
+        <>
+            <h1>Dashboard</h1>
+            <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Default file input example</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group>
+            <Button>Upload</Button>
+        </>
     )
 }
 
