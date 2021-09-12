72c6196133cf5ff1ab8c09daba14e44f7f7dc3d6
puakehaulani <puakehaulani@gmail.com>
2021-09-11 19:48:03 -0700

    transitioned nav to sidebar pkg to stop overflow on links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,34 +1,74 @@
+
 import React from "react";
+import Sidebar from "react-sidebar";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-scroll";
 
-function Navigation() {
-    return (
-        <Navbar bg="transparent" variant="dark" expand="lg" fixed="top">
-            <ul className="flex-column">
-                <li>
-                    <Navbar.Brand href="#">
-                        <img src="../images/logo.png" width="120" height="80" alt="michael scales logo" />
-                    </Navbar.Brand>
-                </li>
 
-                <li>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+const mql = window.matchMedia(`(min-width: 800px)`);
 
-                        <Nav >
-                            <Link to="projects" className="Link">Projects</Link>
-                            <Link to="about" className="Link">About</Link>
-                            <Link to="resume" className="Link">Resume</Link>
-                            <Link to="contact" className="Link">Contact</Link>
-                        </Nav>
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarDocked: mql.matches,
+            sidebarOpen: false
+        };
 
-                    </Navbar.Collapse>
-                </li>
-            </ul>
-        </Navbar>
-    )
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    componentWillMount() {
+        mql.addListener(this.mediaQueryChanged);
+    }
+
+    componentWillUnmount() {
+        this.state.mql.removeListener(this.mediaQueryChanged);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    mediaQueryChanged() {
+        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+    }
+
+    render() {
+        return (
+            <Sidebar
+                sidebar={
+                    <ul className="flex-column">
+                        <li>
+                            <Navbar.Brand href="#">
+                                <img src="../images/logo.png" width="120" height="80" alt="michael scales logo" />
+                            </Navbar.Brand>
+                        </li>
+                        <li className="navbar-nav">
+                            <Nav>
+                                <Link to="projects" className="Link">Projects</Link>
+                                <Link to="about" className="Link">About</Link>
+                                <Link to="resume" className="Link">Resume</Link>
+                                <Link to="contact" className="Link">Contact</Link>
+                            </Nav>
+                        </li>
+                    </ul>
+                }
+                open={this.state.sidebarOpen}
+                docked={true}
+                onSetOpen={this.onSetSidebarOpen}
+                shadow={false}
+                styles={{
+                    sidebar: {
+                        position: "fixed"
+                    }
+                }}
+            >
+            </Sidebar>
+        );
+    }
 }
 
 export default Navigation;
