9fcc1b90f539902c985e9d9ca324d2e17a9cc0d8
puakehaulani <puakehaulani@gmail.com>
2021-09-12 23:41:26 -0700

    cleaned up styling

--- client/src/components/ProjectThumbnail.js
+++ client/src/components/ProjectThumbnail.js
@@ -40,14 +40,12 @@ function Projects() {
 
     async function onDeleteClick(docID) {
         await deleteDoc(doc(db, "projects", docID))
-            .then(
-                alert("byeeeee", docID)
-            );
+
     }
 
     const projThumbs = projects.map((item) =>
-        <Card key={item.title} className="col-3 mx-2 py-1" bg="light" as={Col}>
-            <Card.Img src={item.thumbnail} style={{ height: "90%", width: "100%", objectFit: "cover", borderRadius: 5 }} />
+        <Card key={item.title} className="col-2 m-2 p-1" bg="light" as={Col}>
+            <Card.Img src={item.thumbnail} style={{ height: "90%", objectFit: "cover", borderRadius: 5 }} />
             <Card.Header as="h5" style={{
                 backgroundColor: "transparent", borderColor: "transparent", color: "black", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "baseline"
             }}>
@@ -73,7 +71,7 @@ function Projects() {
 
 
     return (
-        <Card className="m-4 px-4 py-2 col-6" bg="dark">
+        <Card className="m-4 px-4 py-2" bg="dark">
             <h3>Existing projects</h3>
             <Row>
                 {projThumbs}
