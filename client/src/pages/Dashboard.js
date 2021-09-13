7f5dd1e8f403cfe4371d36e0f3a7921f965a8ef5
puakehaulani <puakehaulani@gmail.com>
2021-09-12 17:53:44 -0700

    added button to public site

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import ProjectForm from '../components/ProjectForm';
 
 const Dashboard = () => {
     return (
         <>
-
+            <Link to="/"><img src="../images/logo.png" width="120" height="80" alt="michael scales logo" /></Link>
             <h1>Dashboard</h1>
             <ProjectForm />
         </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Parallax } from 'react-parallax';
 import { Element } from 'react-scroll';
 
