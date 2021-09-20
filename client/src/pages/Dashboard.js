7382dfcf01709aee40882cf91c3b8f3b4a1e0258
puakehaulani <puakehaulani@gmail.com>
2021-09-20 15:12:42 -0700

    added working logout button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import { getAuth, signOut } from "firebase/auth";
+import { FcGoogle } from 'react-icons/fc';
 
 import ProjectForm from '../components/ProjectForm';
 import ProjectThumbnail from '../components/ProjectThumbnail';
 
 const Dashboard = () => {
+
+    const auth = getAuth();
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            alert("byeee")
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
     return (
         <>
-            <Link to="/"><img src="../images/logo.png" width="120" height="80" alt="michael scales logo" /></Link>
+            <Link to="/"><img src="../images/logo.png" width="120" height="80" alt="michael scales logo" /></Link> <Button onClick={handleSignOut} variant="dark" size="lg">
+                <span className="buttonText"><FcGoogle /> Logout</span>
+            </Button>
+
             <h1>Dashboard</h1>
             <Card className="m-4 p-2 col-6" bg="dark">
                 <Card.Header as="h2">Add a Project</Card.Header>
