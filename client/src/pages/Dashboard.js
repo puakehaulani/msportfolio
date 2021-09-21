15da67dbfff7c5a3072a0c3b146f86277a9ab014
puakehaulani <puakehaulani@gmail.com>
2021-09-20 23:48:54 -0700

    refactored, removed comments

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,65 +1,38 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 import { getAuth, signOut } from "firebase/auth";
 import { FcGoogle } from 'react-icons/fc';
 import { Redirect } from 'react-router';
 
-import { UserContext, logOut } from '../providers/UserProvider';
+import { UserContext } from '../providers/UserProvider';
 import ProjectForm from '../components/ProjectForm';
 import ProjectThumbnail from '../components/ProjectThumbnail';
 
 const Dashboard = () => {
     const user = useContext(UserContext);
     const [redirect, setredirect] = useState(null);
-    // const [loggedOut, setLoggedOut] = useState(false)
 
     const auth = getAuth();
     useEffect(() => {
-        console.log("2")
         if (!user) {
-            console.log("2+", user)
             setredirect("/");
         }
-
     }, [user])
-
-    // useEffect(() => {
-    //     if (loggedOut) {
-    //         return (< Redirect to="/" />
-    //         )
-    //     }
-    // }, [loggedOut])
-
-
 
     const handleSignOut = () => {
         signOut(auth).then(() => {
-            // Sign-out successful.
-            // return (<Redirect to={redirect} />)
-            // setLoggedOut(true)
-            // if (user) {
-            //     console.log("hit user")
-            // }
-            // if (redirect) {
-
-            // console.log("5")
-            // return (< Redirect to={redirect} />)
-            // }
             // To Do: LOL FIX THIS
             window.location.reload()
         }).catch((error) => {
-            // An error happened.
             console.log(error.message)
         });
     }
 
     if (redirect) {
-        console.log("4")
         return (<Redirect to={redirect} />)
     }
-    console.log("5")
     return (
         <>
             <Link to="/"><img src="../images/logo.png" width="120" height="80" alt="michael scales logo" /></Link> <Button onClick={handleSignOut} variant="dark" size="lg">
